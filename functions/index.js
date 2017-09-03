83ed537f3fe6da30645ae7d680e4d3d1c62c34dc
Jason Gornall <jason.gornall@lessthan3.com>
2017-09-03 15:17:44 -0700

    bunch of stuff

--- functions/index.js
+++ functions/index.js
@@ -2,42 +2,109 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const request = require('request')
 const btoa = require('btoa')
-var rawjs = require('raw.js');
+const async = require('async')
+const rawjs = require('raw.js');
+const snoowrap = require('snoowrap');
+const lodash = require('lodash');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-// https://www.reddit.com/api/v1/authorize?client_id=Ag0ViT48lPnFiQ&response_type=code&state=123&redirect_uri=https%3A%2F%2Fus-central1-botnet-a2e6f.cloudfunctions.net%2FredditAuth&duration=permanent&scope=vote
-admin.initializeApp(functions.config().firebase);
+// https://www.reddit.com/api/v1/authorize?client_id=Ag0ViT48lPnFiQ&response_type=code&state=123&redirect_uri=https%3A%2F%2Fus-central1-botnet-a2e6f.cloudfunctions.net%2FredditAuth&duration=permanent&scope=vote,identity
+const serviceAccount = require('./service-account.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`
+});
+
+exports.debug = functions.https.onRequest((req, res) => {
+  res.send('hi');
+});
+
+exports.processQueue = functions.https.onRequest((req, res)=> {
+  console.log('start');
+  var queue = [];
+  var upvotes = 0;
+  var active = null;
+  var name = '';
+  var reddit_regex = /^https:\/\/www\.reddit\.com\/r\/(.+?)\/comments\/(.+?)\/.+?\/(.*?)/;
+  admin.database().ref('/queue').limitToFirst(1).once('child_added')
+  .then((snapshot) => {
+    active = snapshot.val()
+    var parts = active.post.match(reddit_regex)
+    if (parts) {
+      name = `t2_${parts[3]}` || `t3_${parts[2]}`
+    }
+    return snapshot.ref.remove()
+  })
+  .then((snapshot) => {
+    return admin.database().ref('/users').once('value')
+  })
+  .then((snapshot) => {
+    return new Promise( (resolve, reject) => {
+      var users = lodash.values(snapshot.val())
+      async.eachLimit(users, 10, function(user, callback) {
+        var reddit = new rawjs("raw.js example script");
+        reddit.setupOAuth2("Ag0ViT48lPnFiQ", "BZPiwonkM5uGdlRK9dfdCz7St6M", "https://us-central1-botnet-a2e6f.cloudfunctions.net/redditAuth");
+        reddit.refreshToken = user.refresh_token
+        reddit.auth(function(err, response) {
+          request.post({
+            headers: {
+              'content-type' : 'application/x-www-form-urlencoded',
+              "authorization": "Bearer " + response.access_token,
+              "User-Agent": 'raw.js example script'
+            },
+            url:     'https://oauth.reddit.com/api/vote',
+            body:    `id=${name}&dir=1`
+          }, function(error, response, body) {
+            upvotes += 1;
+            callback(null)
+          });
+        })
+      }, (err, data) => {
+        if (err) {return reject()}
+        resolve()
+      })
+    })
+  })
+  .then((a,b) => {
+    return admin.database().ref('/posts').push({
+      url: active,
+      upvotes: upvotes
+    })
+  })
+  .then((snapshot) => {
+    res.send('ok');
+  })
+  .catch(error => {
+    res.send(`${error} occured`)
+  })
+})
 exports.redditAuth = functions.https.onRequest((req, res) => {
   var reddit = new rawjs("raw.js example script");
   reddit.setupOAuth2("Ag0ViT48lPnFiQ", "BZPiwonkM5uGdlRK9dfdCz7St6M", "https://us-central1-botnet-a2e6f.cloudfunctions.net/redditAuth");
   var url = reddit.authUrl("123", ['vote identity']);
   reddit.auth({"code": req.query.code}, function(err, response) {
-    console.log(response, '123');
-    reddit.me(function(err, response) {
-      console.log(response, '456');
+    reddit.me(function(err, user) {
+      var uid = user.name;
+      var refresh_token = response.refresh_token;
+      var access_token = response.access_token;
 
-      admin.auth().createUser({
-        uid: uid,
-        displayName: displayName,
-        photoURL: photoURL
-      });
+
+      return admin.database().ref(`/users/${uid}`).set({
+        refresh_token: refresh_token,
+        access_token: access_token
+      })
+      .then(() => {
+        return admin.auth().createCustomToken(uid)
+      })
+
+      .then(customToken => {
+        res.redirect(`http://127.0.0.1:4000/?token=${customToken}`);
+      })
+      .catch(error => {
+        res.send(`${error} occured`)
+      })
+       res.send('hi');
     })
-    res.send(response);
   })
-  // request.post({
-  //   headers: {
-  //     'Authorization': 'Basic ' + header,
-  //     'User-Agent': "REDDIT posTer"
-  //   },
-  //   url: 'https://www.reddit.com/api/v1/access_token',
-  //   form: {
-  //     grant_type: 'authorization_code',
-  //     code: req.query.code,
-  //     redirect_uri: 'https://us-central1-botnet-a2e6f.cloudfunctions.net/redditAuth'
-  //   }
-  // }, function(err,httpResponse,body){
-  //   console.log(body);
-  //   response.send("Hello from Firebase!" + err);
-  // })
 });
--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -1,13 +1,51 @@
 $(window).load ->
+  token = $.url '?token'
+  if token
+    firebase.auth().signInWithCustomToken token
+    updateQueryStringParam 'token'
+
   firebase.auth().onAuthStateChanged (user) ->
-    $('.reddit-login').on 'click', (e) ->
-      params = ("#{k}=#{encodeURIComponent v}" for k, v of {
-        client_id: 'Ag0ViT48lPnFiQ'
-        response_type: 'code'
-        state: '123'
-        redirect_uri: 'https://us-central1-botnet-a2e6f.cloudfunctions.net/redditAuth'
-        duration: 'permanent'
-        scope: 'vote identity'
-      }).join '&'
-      url = "https://www.reddit.com/api/v1/authorize?#{params}"
-      window.open(url)
+    $('#user').empty()
+    return unless user
+    window.logged_in_user = user
+
+    firebase.database().ref("posts").on 'child_added', (item) ->
+      $('#posts').append teacup.render ->
+        div '.post', ->
+          div '.upvotes', -> item.upvotes
+          blockquote ".reddit-card", ->
+            a href: "#{item.post}/?ref=share&ref_source=embed"
+
+    firebase.database().ref("users/#{user.uid}").on 'value', (doc) ->
+      $('#user').html teacup.render ->
+        div '.user', ->
+          div -> "User: #{user.uid}"
+        div '.post', ->
+          span ->'Reddit Post: '
+          input value: doc.child('post').val()
+        div '.comment', ->
+          span -> 'Comment Post: '
+          input value: doc.child('comment').val()
+        div '.button', -> 'Submit'
+
+      $('#user .button').on 'click', (e) ->
+        current_user = doc.val()
+        current_user.comment = $('#user .comment input').val()
+        current_user.post = $('#user .post input').val()
+        doc.ref.set current_user, ->
+          firebase.database().ref("queue").push({
+            user: user.uid,
+            post: current_user.post
+          })
+
+  $('.reddit-login').on 'click', (e) ->
+    params = ("#{k}=#{encodeURIComponent v}" for k, v of {
+      client_id: 'Ag0ViT48lPnFiQ'
+      response_type: 'code'
+      state: '123'
+      redirect_uri: 'https://us-central1-botnet-a2e6f.cloudfunctions.net/redditAuth'
+      duration: 'permanent'
+      scope: 'vote identity'
+    }).join '&'
+    url = "https://www.reddit.com/api/v1/authorize?#{params}"
+    window.open(url)
